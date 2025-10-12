6a9fb8c46b923ef56f6f46e259937a53f52a5160
puudelkoo <147343436+puudelkoo@users.noreply.github.com>
2025-10-12 14:16:42 +0200

    Update zad5l.sql

--- 01_setup/zad5l.sql
+++ 01_setup/zad5l.sql
@@ -1,4 +1,7 @@
---Policz sumę wszystkich wynagrodzeń
-SELECT SUM(ksiegowosc.pensja.kwota) as suma_wynagrodzen
-FROM ksiegowosc.wynagrodzenia
-JOIN ksiegowosc.pensja ON ksiegowosc.wynagrodzenia.id_pensji = ksiegowosc.pensja.id_pensji;
+--Policz średnią, minimalną i maksymalną płacę dla stanowiska ‘kierownik’
+SELECT
+    AVG(ksiegowosc.pensja.kwota) AS srednia_placa_Kierownik,
+    MIN(ksiegowosc.pensja.kwota) AS min_placa_Kierownik,
+    MAX(ksiegowosc.pensja.kwota) AS max_placa_Kierownik
+FROM ksiegowosc.pensja
+WHERE ksiegowosc.pensja.stanowisko = 'Kierownik';
