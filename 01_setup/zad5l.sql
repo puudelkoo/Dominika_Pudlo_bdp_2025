b91b3c05cd5ae72d7fca2a40abc6a54edff5e727
puudelkoo <147343436+puudelkoo@users.noreply.github.com>
2025-10-12 14:07:05 +0200

    Update zad5l.sql

--- 01_setup/zad5l.sql
+++ 01_setup/zad5l.sql
@@ -1,1 +1,4 @@
-
+--Policz sumę wszystkich wynagrodzeń
+SELECT SUM(ksiegowosc.pensja.kwota) as suma_wynagrodzen
+FROM ksiegowosc.wynagrodzenia
+JOIN ksiegowosc.pensja ON ksiegowosc.wynagrodzenia.id_pensji = ksiegowosc.pensja.id_pensji;
