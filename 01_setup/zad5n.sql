027c6a01aff1b9da3711942e391829accb47dec0
puudelkoo <147343436+puudelkoo@users.noreply.github.com>
2025-10-12 14:07:46 +0200

    Update zad5n.sql

--- 01_setup/zad5n.sql
+++ 01_setup/zad5n.sql
@@ -1,1 +1,8 @@
-
+--Wyznacz liczbe premii przyznanych dla pracowników danego stanowiska
+SELECT
+    ksiegowosc.pensja.stanowisko,
+    COUNT(ksiegowosc.premia.id_premii) AS liczba_premii
+FROM ksiegowosc.wynagrodzenia
+LEFT JOIN ksiegowosc.pensja ON ksiegowosc.wynagrodzenia.id_pensji = ksiegowosc.pensja.id_pensji
+LEFT JOIN ksiegowosc.premia ON ksiegowosc.wynagrodzenia.id_premii = ksiegowosc.premia.id_premii
+GROUP BY ksiegowosc.pensja.stanowisko;
