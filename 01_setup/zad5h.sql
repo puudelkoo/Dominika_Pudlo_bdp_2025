773002a0682a4f85427f0f9a63dbbc9e311c12e4
puudelkoo <147343436+puudelkoo@users.noreply.github.com>
2025-10-12 14:15:18 +0200

    Update zad5h.sql

--- 01_setup/zad5h.sql
+++ 01_setup/zad5h.sql
@@ -1,6 +1,10 @@
---Uszereguj pracowników według pensji
-SELECT imie, nazwisko, ksiegowosc.pensja.kwota as pensja
-FROM ksiegowosc.pracownicy
-JOIN ksiegowosc.wynagrodzenia ON ksiegowosc.pracownicy.id_pracownika = ksiegowosc.wynagrodzenia.id_pracownika
-JOIN ksiegowosc.pensja ON ksiegowosc.wynagrodzenia.id_pensji = ksiegowosc.pensja.id_pensji
-ORDER BY ksiegowosc.pensja.kwota;
+--Wyświetl imię i nazwisko pracowników, którzy pracowali w nadgodzinach i nie otrzymali premii
+SELECT DISTINCT p.imie, p.nazwisko
+FROM ksiegowosc.pracownicy p
+JOIN ksiegowosc.wynagrodzenia w ON p.id_pracownika = w.id_pracownika
+JOIN ksiegowosc.godziny g 
+      ON w.id_pracownika = g.id_pracownika 
+     AND w.data = g.data
+JOIN ksiegowosc.premia pr ON w.id_premii = pr.id_premii
+WHERE g.liczba_godzin > 160  
+  AND pr.kwota = 0; 
