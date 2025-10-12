96a493ca6bcdc943a07535535ed230204bf7eef1
puudelkoo <147343436+puudelkoo@users.noreply.github.com>
2025-10-12 14:16:03 +0200

    Update zad5j.sql

--- 01_setup/zad5j.sql
+++ 01_setup/zad5j.sql
@@ -1,4 +1,8 @@
---Zlicz i pogrupuj pracowników według pola ‘stanowisko’
-SELECT stanowisko, COUNT(*) 
-FROM ksiegowosc.pensja
-GROUP BY ksiegowosc.pensja.stanowisko;
+--Uszereguj pracowników według pensji i premii malejąco. 
+SELECT 
+    p.imie, p.nazwisko,(pe.kwota + pr.kwota) AS laczne_wynagrodzenie
+FROM ksiegowosc.pracownicy p
+JOIN ksiegowosc.wynagrodzenia w ON p.id_pracownika = w.id_pracownika
+JOIN ksiegowosc.pensja pe ON w.id_pensji = pe.id_pensji
+JOIN ksiegowosc.premia pr ON w.id_premii = pr.id_premii
+ORDER BY laczne_wynagrodzenie DESC;
