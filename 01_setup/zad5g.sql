023f944cdfa7982273f6a8a289ee5136c05cdbcd
puudelkoo <147343436+puudelkoo@users.noreply.github.com>
2025-10-12 14:13:56 +0200

    Update zad5g.sql

--- 01_setup/zad5g.sql
+++ 01_setup/zad5g.sql
@@ -1,10 +1,6 @@
---Wyświetla imię i nazwisko pracowników oraz liczbę ich nadgodzin, przyjmując, iż standardowy czas pracy to 160h miesięcznie. 
-SELECT DISTINCT p.imie, p.nazwisko
+--Wyświetla imie i nazwisko pracowników, których pensja zawiera się w przedziale 1500-3000 PLN
+SELECT p.imie, p.nazwisko
 FROM ksiegowosc.pracownicy p
 JOIN ksiegowosc.wynagrodzenia w ON p.id_pracownika = w.id_pracownika
-JOIN ksiegowosc.godziny g 
-      ON w.id_pracownika = g.id_pracownika 
-     AND w.data = g.data
-JOIN ksiegowosc.premia pr ON w.id_premii = pr.id_premii
-WHERE g.liczba_godzin > 160  
-  AND pr.kwota = 0;  
+JOIN ksiegowosc.pensja pe ON w.id_pensji = pe.id_pensji
+WHERE pe.kwota BETWEEN 1500 AND 3000;
