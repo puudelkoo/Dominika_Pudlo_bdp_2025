44b6c73cd0890934a2aefafc21695393d61c96c0
puudelkoo <147343436+puudelkoo@users.noreply.github.com>
2025-10-12 14:06:11 +0200

    Update zad5i.sql

--- 01_setup/zad5i.sql
+++ 01_setup/zad5i.sql
@@ -1,1 +1,8 @@
-
+--Uszereguj pracowników według pensji i premii malejąco. 
+SELECT 
+    p.imie, p.nazwisko,(pe.kwota + pr.kwota) AS laczne_wynagrodzenie
+FROM ksiegowosc.pracownicy p
+JOIN ksiegowosc.wynagrodzenia w ON p.id_pracownika = w.id_pracownika
+JOIN ksiegowosc.pensja pe ON w.id_pensji = pe.id_pensji
+JOIN ksiegowosc.premia pr ON w.id_premii = pr.id_premii
+ORDER BY laczne_wynagrodzenie DESC;
