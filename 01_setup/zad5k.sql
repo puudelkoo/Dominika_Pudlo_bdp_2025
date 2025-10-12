c15a6f6d5c428ac8ef44dfcaf3854516ca17a5bf
puudelkoo <147343436+puudelkoo@users.noreply.github.com>
2025-10-12 14:16:25 +0200

    Update zad5k.sql

--- 01_setup/zad5k.sql
+++ 01_setup/zad5k.sql
@@ -1,7 +1,4 @@
---Policz średnią, minimalną i maksymalną płacę dla stanowiska ‘kierownik’
-SELECT
-    AVG(ksiegowosc.pensja.kwota) AS srednia_placa_Kierownik,
-    MIN(ksiegowosc.pensja.kwota) AS min_placa_Kierownik,
-    MAX(ksiegowosc.pensja.kwota) AS max_placa_Kierownik
+--Zlicz i pogrupuj pracowników według pola ‘stanowisko’
+SELECT stanowisko, COUNT(*) 
 FROM ksiegowosc.pensja
-WHERE ksiegowosc.pensja.stanowisko = 'Kierownik';
+GROUP BY ksiegowosc.pensja.stanowisko;
