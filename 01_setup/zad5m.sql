d1bdcb23c8c347ba05af38b2efead6264306effa
puudelkoo <147343436+puudelkoo@users.noreply.github.com>
2025-10-12 14:07:31 +0200

    Update zad5m.sql

--- 01_setup/zad5m.sql
+++ 01_setup/zad5m.sql
@@ -1,1 +1,7 @@
-
+--Policz sumę wynagrodzeń w ramach danego stanowiska
+SELECT stanowisko,
+    SUM(pensja.kwota) + COALESCE(SUM(premia.kwota), 0) AS suma_wynagrodzen
+FROM ksiegowosc.wynagrodzenia
+LEFT JOIN ksiegowosc.pensja ON ksiegowosc.wynagrodzenia.id_pensji = ksiegowosc.pensja.id_pensji
+LEFT JOIN ksiegowosc.premia ON ksiegowosc.wynagrodzenia.id_premii = ksiegowosc.premia.id_premii
+GROUP BY ksiegowosc.pensja.stanowisko;
