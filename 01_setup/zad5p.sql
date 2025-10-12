cb11998185233a5c6abbc9050dc6dbbb106ef3a3
puudelkoo <147343436+puudelkoo@users.noreply.github.com>
2025-10-12 14:08:25 +0200

    Update zad5p.sql

--- 01_setup/zad5p.sql
+++ 01_setup/zad5p.sql
@@ -1,1 +1,6 @@
-
+--Usuwanie wszystkich pracowników mających pensję mniejszą niż 1200 zł
+DELETE FROM ksiegowosc.wynagrodzenia
+WHERE id_pensji IN 
+	(SELECT id_pensji
+    FROM ksiegowosc.pensja
+    WHERE kwota < 1200);
