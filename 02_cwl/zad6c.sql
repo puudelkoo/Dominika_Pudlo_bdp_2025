8db4b693e8a297cc8a8560021544b703e33efc26
puudelkoo <147343436+puudelkoo@users.noreply.github.com>
2025-10-21 12:04:53 +0200

    Create zad6c.sql

---
+++ 02_cwl/zad6c.sql
@@ -0,0 +1,8 @@
+--c. Wypisz nazwy i pola powierzchni wszystkich poligonów w warstwie budynki.
+--Wyniki posortuj alfabetycznie. 
+
+SELECT 
+    name,
+    ST_Area(geometry) AS area
+FROM buildings
+ORDER BY name;
