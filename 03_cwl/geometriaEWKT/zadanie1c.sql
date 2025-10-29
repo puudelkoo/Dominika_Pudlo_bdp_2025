4e4222c74da528cf7499f37f85fdb7f4ec61be02
puudelkoo <147343436+puudelkoo@users.noreply.github.com>
2025-10-29 12:01:27 +0100

    Create zadanie1c.sql

---
+++ 03_cwl/geometriaEWKT/zadanie1c.sql
@@ -0,0 +1,13 @@
+--c
+INSERT INTO obiekty (nazwa, geom)
+VALUES (
+    'obiekt3',
+    ST_GeomFromText(
+        'POLYGON((
+            7 15,
+            10 17,
+            12 13,
+            7 15
+        ))'
+    )
+);
