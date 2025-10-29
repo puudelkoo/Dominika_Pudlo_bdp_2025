21acbd8f2abc3e95f893bf562fcd65abfc7144ef
puudelkoo <147343436+puudelkoo@users.noreply.github.com>
2025-10-29 12:02:32 +0100

    Create zadanie1f.sql

---
+++ 03_cwl/geometriaEWKT/zadanie1f.sql
@@ -0,0 +1,11 @@
+--f
+INSERT INTO obiekty (nazwa, geom)
+VALUES (
+    'obiekt6',
+    ST_GeomFromText(
+        'GEOMETRYCOLLECTION(
+            LINESTRING(1 1, 3 2),
+            POINT(4 2)
+        )'
+    )
+);
